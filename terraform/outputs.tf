7f680229142671e9723e576902842fa636731c37
Nikita Desai <desainikita7899@gmail.com>
2026-05-30 00:43:30 +0530

    Debug: Print live container startup logs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,3 @@
 output "ec2_public_ip" {
-  description = "The public IP address of our production server"
-  value       = aws_instance.smart_scrap_server.public_ip
+  value = aws_instance.smart_scrap_server.public_ip
 }
